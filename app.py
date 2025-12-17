f66ab16c54c2c9f9c69de45b6d2c53d6ce3b7c56
spmur15 <93347066+spmur15@users.noreply.github.com>
2025-12-16 22:11:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -523,9 +523,9 @@ def load_team_data(team):
     """
     Load shot data for selected team.
     Expected filename format:
-    Shot Location Data/{Team}_shot_data_2025.csv
+    Shot Location Data/{Team}_shot_data_2026.csv
     """
-    path = f"Shot Location Data//{team}_shot_data_2025.csv"
+    path = f"Shot Location Data//{team}_shot_data_2026.csv"
     dff = pd.read_csv(path)
 
     #print(team)
