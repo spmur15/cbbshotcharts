5db7e6fc6e61a96c06a3806c99a3cf20225aab8d
spmur15 <sammurray826@gmail.com>
2026-01-28 22:16:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2159,7 +2159,7 @@ app.layout = dbc.Container(
                                                     "fontWeight": "600",
                                                 },
                                             ),
-                                            width =10#xs=10, md=10
+                                            width = 9 #xs=10, md=10
                                         ),
 
                                         dbc.Col(
@@ -2171,7 +2171,7 @@ app.layout = dbc.Container(
                                                 size='sm'
                                             ),
                                             width=3,  
-                                            className="px-3"  # ✅ adds padding on sides
+                                            #className="px-3"  # ✅ adds padding on sides
                                         ),
 
                                         #], justify='left'),
