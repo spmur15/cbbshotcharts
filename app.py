aae3a69396d9b1a3b0de807132335c5914837720
spmur15 <sammurray826@gmail.com>
2026-02-23 14:56:32 -0600

    Data update feb 23

--- app.py
+++ app.py
@@ -2220,7 +2220,7 @@ app.layout = dbc.Container(
                         }
                     ),
                     html.Div(
-                        "Games thru Feb. 15",
+                        "Games thru Feb. 23",
                         className="text-center",
                         style={
                             "fontSize": "12.5px",
